fa3534d5d9070798fd2e418d271ddcd67dad6cb2
khl6235 <khl6235@naver.com>
2020-04-25 23:24:07 +0900

    level 2

---
+++ homework2/hw2practice/routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.use('/blog', require('./blog'));
+router.use('/post', require('./post'));
+router.use('/api', require('./api'));
+
+module.exports = router;
